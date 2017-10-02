69471e70dd10d986b984e3dae4d3445cf1379f3c
nayunhwan <nayunhwan.dev@gmail.com>
2017-10-03 01:48:38 +0900

    fix(index): remove 'User' and add 'Comment'

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 
+const Comment = require('./Comment');
 const NaverMovie = require('./NaverMovie');
 const OpenMovie = require('./OpenMovie');
 const TMDB = require('./TMDB');
-const User = require('./User');
 
 
+router.use('/Comment', Comment);
 router.use('/NaverMovie', NaverMovie);
 router.use('/OpenMovie', OpenMovie);
 router.use('/TMDB', TMDB);
-router.use('/User', User);
 
 module.exports = router;
