d93259fb19c04cc49ec467613a70b59283517d06
nayunhwan <nayunhwan.dev@gmail.com>
2017-11-26 02:22:15 +0900

    feat(TMDB): create function to get Boxoffice from TMDB

--- routes/api/TMDB/TMDB.controller.js
+++ routes/api/TMDB/TMDB.controller.js
@@ -1,11 +1,18 @@
 const request = require('request');
 const qs = require('querystring');
+const key = '083f7cf05368860a32210b553f6a0a23';
 
 exports.tmdbSearch = (req, res) => {
 	const query = require('url').parse(req.url, true).query;
-	const key = '083f7cf05368860a32210b553f6a0a23';
 	const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=ko&query=${qs.escape(query.movieName)}&page=1&include_adult=false`;
 	request.get(url, function(err, result, body) {
 		res.send(body);
 	});
+}
+
+exports.getBoxoffice = (req, res) => {
+	const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=ko&page=1`;
+	request.get(url, (err, result, body) => {
+		res.send(body);
+	})
 }
--- routes/api/TMDB/index.js
+++ routes/api/TMDB/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('./TMDB.controller');
 
 router.get('/search', controller.tmdbSearch);
+router.get('/getBoxoffice', controller.getBoxoffice);
 
 module.exports = router;
