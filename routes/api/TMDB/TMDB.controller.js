361e704b1a1bb0f91817e563a60561c5cb6bc4da
nayunhwan <nayunhwan.dev@gmail.com>
2017-11-26 02:23:28 +0900

    fix(TMDB): make anonymous function to arrow function in TMDB

--- routes/api/TMDB/TMDB.controller.js
+++ routes/api/TMDB/TMDB.controller.js
@@ -5,7 +5,7 @@ const key = '083f7cf05368860a32210b553f6a0a23';
 exports.tmdbSearch = (req, res) => {
 	const query = require('url').parse(req.url, true).query;
 	const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=ko&query=${qs.escape(query.movieName)}&page=1&include_adult=false`;
-	request.get(url, function(err, result, body) {
+	request.get(url, (err, result, body) => {
 		res.send(body);
 	});
 }
